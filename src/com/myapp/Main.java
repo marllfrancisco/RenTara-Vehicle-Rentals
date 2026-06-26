69848b8599b090f53464552a8acb6ec80e23522c
unknown <ardanimelencio@gmail.com>
2026-06-26 21:27:00 +0800

    feat(main): test db

---
+++ src/com/myapp/Main.java
@@ -0,0 +1,12 @@
+package src.com.myapp;
+
+import src.database_management.Database;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("a");
+
+        Database db = new Database("rentaran.db");
+        db.try_connect();
+    }
+}
