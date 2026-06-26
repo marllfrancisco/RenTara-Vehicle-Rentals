1e55a2fb34ff32c7964c0687536589e15cde2a11
unknown <ardanimelencio@gmail.com>
2026-06-26 23:23:27 +0800

    feat(menu): Created Default Main Menu

---
+++ src/menus/MainMenu.java
@@ -0,0 +1,19 @@
+package menus;
+
+import database_management.Database;
+
+public class MainMenu {
+    Database db;
+
+    public MainMenu(Database db){
+        this.db = db;
+    }
+
+    public void start(){
+
+        // Implement while loop until user presses designated exit button
+        // Prompts user to select between menu selections
+
+        System.out.println("Test");
+    }
+}
