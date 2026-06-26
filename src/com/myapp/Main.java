5b83e86862c0014317c2bb9d89a2c6de716c4277
unknown <ardanimelencio@gmail.com>
2026-06-26 23:24:34 +0800

    feat(main): implement login->menu process

--- src/com/myapp/Main.java
+++ src/com/myapp/Main.java
@@ -1,11 +1,18 @@
-package src.com.myapp;
+package com.myapp;
 
-import src.database_management.Database;
+import database_management.Database;
+import menus.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("a");
-
         Database db = new Database("RenTara.db");
+
+        Login login = new Login(db);
+        MainMenu main_menu = new MainMenu(db);
+        if (login.start() == true) {
+            main_menu.start();
+        }
+
+        System.out.print("\033[H\033[2J");
     }
 }
