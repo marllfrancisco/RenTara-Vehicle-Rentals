6938970554c771154675a6cc781aefa4a1d79eaa
unknown <ardanimelencio@gmail.com>
2026-06-26 23:23:39 +0800

    feat(menu): Created Default Login Menu

--- src/menus/Login.java
+++ src/menus/Login.java
@@ -1,8 +1,21 @@
-package src.menus;
+package menus;
+
+import database_management.Database;
 
 public class Login {
 
-    public void Test(){
+    Database db;
+
+    public Login(Database db){
+        this.db = db;
+    }
+
+    public boolean start(){
+
+        // Implement 3 attempt registry using while loop
+        // Use db connection to compare input and available records via SELECT FROM WHERE
         System.out.println("Test");
+
+        return true;
     }
 }
