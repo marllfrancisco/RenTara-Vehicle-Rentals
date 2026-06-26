c66cfe0427781de8ccd869d1af37b18356b542ce
unknown <ardanimelencio@gmail.com>
2026-06-26 22:22:58 +0800

    fix: RenTara instead of rentaran

--- src/com/myapp/Main.java
+++ src/com/myapp/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("a");
 
-        Database db = new Database("rentaran.db");
+        Database db = new Database("RenTara.db");
     }
 }
