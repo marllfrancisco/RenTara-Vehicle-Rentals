161d4929eec260b51c51ebd9b5b26b9864ef1e18
unknown <ardanimelencio@gmail.com>
2026-06-26 22:15:14 +0800

    refactor(db): Proper table initiation

--- src/database_management/Database.java
+++ src/database_management/Database.java
@@ -7,7 +7,9 @@ public class Database {
     Connection conn;
     
     public Database(String filename) {
-        url = "jdbc:sqlite:" + filename;
+        url = "jdbc:sqlite:data/" + filename;
+        try_connect();
+        init_tables();
     }
 
     public void try_connect() {
@@ -21,8 +23,15 @@ public class Database {
     }
 
     public void init_tables() {
-        String account_statement = """
-                CREATE TABLE IF NOT EXISTS 
-                """;
+
+        try {
+            Statement statement = conn.createStatement();
+            statement.execute(Init_Constants.CREATE_USERS_TABLE);
+            statement.execute(Init_Constants.CREATE_VEHICLES_TABLE);
+            statement.execute(Init_Constants.CREATE_BOOKINGS_TABLE);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
