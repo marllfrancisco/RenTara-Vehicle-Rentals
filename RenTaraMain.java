74e81e4010a5b5783eb78ee5daaf911fe21b86f4
unknown <ardanimelencio@gmail.com>
2026-06-26 20:09:50 +0800

    feat: Entry Script

---
+++ RenTaraMain.java
@@ -0,0 +1,9 @@
+import menus.Login;
+
+public class RenTaraMain {
+    public static void main(String[] args) {
+
+        Login login = new Login();
+        login.Test();
+    }
+}
---
+++ menus/Login.java
@@ -0,0 +1,8 @@
+package menus;
+
+public class Login {
+
+    public void Test(){
+        System.out.println("Test");
+    }
+}
