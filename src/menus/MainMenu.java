8aecfaa5eebb405859c7de975529d9f0556c7276
unknown <ardanimelencio@gmail.com>
2026-06-27 13:40:27 +0800

    feat(menu): implement multiple functions Separate Admin and Customer UI Admin:     Switch KYC (implemented)     View all (implemented)     Report Gen (WIP) Customer:     ALL ARE WIP

--- src/menus/MainMenu.java
+++ src/menus/MainMenu.java
@@ -1,8 +1,13 @@
 package menus;
 
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 import database_management.Database;
+import models.User;
+import models.UserRepository;
 
 public class MainMenu {
     Database db;
@@ -13,16 +18,23 @@ public class MainMenu {
 
     public void admin_start(){
         Scanner scan = new Scanner(System.in);
+        AdminMenu menu = new AdminMenu(db, scan);
 
         while(true) {
             System.out.print("\033[H\033[2J");
             
-            System.out.print(MenuStrings.MENU_STRING);
+            System.out.print(MenuStrings.ADMIN_STRING);
             String ans = scan.nextLine();
             switch (ans) {
                 case "1":
+                    menu.approveKYC();
                     break;
-                case "5":
+                case "2":
+                    menu.viewAll();
+                    break;
+                case "3":
+                    break;
+                case "4":
                     scan.close();
                     return;
                 default:
@@ -33,6 +45,99 @@ public class MainMenu {
     }
 
     public void customer_start() {
+        Scanner scan = new Scanner(System.in);
+        //CustomerMenu menu = new CustomerMenu();
 
+        while(true) {
+            System.out.print("\033[H\033[2J");
+            
+            System.out.print(MenuStrings.CUSTOMER_STRING);
+            String ans = scan.nextLine();
+            switch (ans) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":   
+                    scan.close();
+                    return;
+                default:
+                    break;
+            }
+
+        }
     }
+}
+
+class AdminMenu {
+
+    Database db;
+    Scanner scan; 
+    public AdminMenu(Database db, Scanner scan) { this.db = db; this.scan = scan;}
+
+    private String formatAString(Integer ID, String username, Boolean approved) {
+        String idFormatted = String.format("%-5s", ID);
+        String nameFormatted = String.format("%-30s", username);
+        String approvedFormatted = String.format("%-10s", approved);
+
+        return idFormatted + nameFormatted + approvedFormatted;
+    }
+
+
+    public void approveKYC() {
+
+        UserRepository urepo = new UserRepository(db.getConnection());
+
+        try {
+            List<User> users = urepo.findAll();
+
+            System.out.print("\033[H\033[2J");
+            System.out.println("User List");
+            System.out.println("User List\nID         NAME                 APPROVED");
+            for (int i = 0; i < users.size(); i++) {
+                if (!users.get(i).isAdmin()) {
+                    System.out.println(formatAString(users.get(i).getId(), users.get(i).getFullName(), users.get(i).isKycApproved()));
+                }
+            }
+
+            System.out.print("Enter ID: ");
+            int ID = scan.nextInt();
+            
+            Optional<User> search = urepo.findById(ID);
+            if (search.isPresent()) {
+                User user = search.get();
+                if (!user.isAdmin()){ // Prevent admin tampering
+                    user.switchKycApproved();
+                    urepo.update(user);
+                }
+            }
+            System.out.println("Press ENTER to continue");
+            scan.nextLine();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void viewAll() {
+
+        UserRepository urepo = new UserRepository(db.getConnection());
+
+        try {
+            List<User> users = urepo.findAll();
+
+            System.out.print("\033[H\033[2J");
+            System.out.println("User List\nID         NAME                 APPROVED");
+            for (int i = 0; i < users.size(); i++) {
+                System.out.println(formatAString(users.get(i).getId(), users.get(i).getFullName(), users.get(i).isKycApproved()));
+            }
+
+            System.out.print("Press ENTER to continue");
+            scan.nextLine();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
