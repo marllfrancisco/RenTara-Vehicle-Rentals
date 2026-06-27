3b6dbbccc21515f2e2183d7d02b2d65395fa7f7f
unknown <ardanimelencio@gmail.com>
2026-06-27 13:39:17 +0800

    feat(menu): add customer and login constant

--- src/menus/MenuStrings.java
+++ src/menus/MenuStrings.java
@@ -3,9 +3,24 @@ package menus;
 public class MenuStrings {
     private MenuStrings(){}
     
-    public static final String MENU_STRING = """
+    public static final String LOGIN_STRING = """
+            === LOGIN/REGISTER ===
+            1. Log in
+            2. Register
+            Entry: """;
+
+    public static final String ADMIN_STRING = """
             === ADMIN MAIN MENU ===
-            1. User Menu
-            2. Car Menu
+            1. Approve KYC
+            2. View All Users
+            3. View Revenue Report
+            4. Exit
+            Entry: """;
+
+    public static final String CUSTOMER_STRING = """
+            === CUSTOMER MAIN MENU ===
+            1. Rent a Vehicle
+            2. List my Vehicle for Rent
+            3. Exit
             Entry: """;
 }
