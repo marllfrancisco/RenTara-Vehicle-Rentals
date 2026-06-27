83c4bd5f0d88587b95d94fcf4028e310f310bca9
unknown <ardanimelencio@gmail.com>
2026-06-27 13:55:17 +0800

    feat(menu): add customer menu (WIP)

---
+++ src/menus/CustomerMenu.java
@@ -0,0 +1,29 @@
+package menus;
+
+import java.util.Scanner;
+
+import database_management.Database;
+
+public class CustomerMenu {
+
+    Database db;
+    Scanner scan; 
+    public CustomerMenu(Database db, Scanner scan) { this.db = db; this.scan = scan;}
+
+    public void rentVehicle() {
+
+    }
+
+    public void returnRent() {
+
+    }
+ 
+    public void addVehicle() {
+
+    }
+
+    public void unlistVehicle() {
+
+    }
+
+}
