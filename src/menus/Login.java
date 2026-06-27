eb13c09d4c1e8a41eb3c61a4375b4dd202397fd4
unknown <ardanimelencio@gmail.com>
2026-06-27 11:15:25 +0800

    feat(menu): add login

--- src/menus/Login.java
+++ src/menus/Login.java
@@ -2,20 +2,68 @@ package menus;
 
 import database_management.Database;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class Login {
 
     Database db;
+    Scanner scan = new Scanner(System.in);
 
     public Login(Database db){
         this.db = db;
     }
 
-    public boolean start(){
+    public String ask_info(String question) {
+        System.out.print("Enter " + question + ":");
+        return scan.nextLine();
+    }
 
-        // Implement 3 attempt registry using while loop
-        // Use db connection to compare input and available records via SELECT FROM WHERE
-        System.out.println("Test");
+    public String compare_input(String user, String pass) throws SQLException {
 
-        return true;
+        try {
+            PreparedStatement statement = db.getConnection().prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
+            statement.setString(1, user);
+            statement.setString(2, pass);
+
+            ResultSet rs = statement.executeQuery();
+            if (!rs.next()) {
+                throw new SQLException();
+            }
+            else {
+                return rs.getString(3);
+            }
+        }
+        catch (SQLException e) {
+            throw new SQLException();
+        }
+    }
+
+    public String start(){
+
+
+        int attempts = 3;
+
+        while (attempts != 0) {
+
+            System.out.print("\033[H\033[2J");
+
+            String user = ask_info("username");
+            String pass = ask_info("password");
+
+            try {
+                String value = compare_input(user, pass);
+                if (value != null) return value;
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+                attempts -= 1;
+            }
+        }   
+        scan.close();
+        return "None";
     }
 }
